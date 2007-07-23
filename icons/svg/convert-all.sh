154c0cfc14614a28730476a7e65bee3481d1da94
atomo64 <atomo64>
2007-07-23 22:14:54 +0000

    Updated default icon

--- icons/svg/convert-all.sh
+++ icons/svg/convert-all.sh
@@ -3,6 +3,6 @@
 for e in 16 22 32 48 64 128 ; do
 	for r in kcheckgmail kcheckgmaillight kcheckgmailauth ; do
 		rm -f ../cr$e-app-$r.png
-		inkscape -z -f $r.svg -e ../cr$e-app-$r.png -b white -w $e -h $e
+		inkscape -z -f $r.svg -e ../cr$e-app-$r.png -b white -w $e -h $e -y 0.0
 	done
 done
