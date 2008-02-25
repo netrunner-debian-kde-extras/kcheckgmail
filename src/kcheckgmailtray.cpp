43ef75652afe3936e337a5fe3f4cb40939610637
luispereira <luispereira>
2008-02-25 12:25:00 +0000

    Merged from branch kcheckgmail-0_5_7_x. Adding missing #include <cstdlib>, fixing a FTBFS with GCC 4.3 (Closes #456050 in DebBTS)
    Author: atomo64 <atomo64>
    Committer: luispereira <luispereira>

--- src/kcheckgmailtray.cpp
+++ src/kcheckgmailtray.cpp
@@ -19,6 +19,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstdlib>
 #include <kaction.h>
 #include <kapplication.h>
 #include <kconfigdialog.h>
