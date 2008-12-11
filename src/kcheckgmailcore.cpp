147f28a9e3957e216ccd5865b1a1c8505f2fb7af
Raphael Geissert <atomo64@gmail.com>
2008-12-11 23:22:08 +0000

    Re set the check interval only after all the settings were checked. This prevents some weird behaviours when the configuration was not correct but an email check was triggered.
    Conflicts:
    
    	src/kcheckgmailtray.cpp
    (cherry picked from commit dec5e1bc6c926f88b7c1715d3410f346de384150)

--- src/kcheckgmailcore.cpp
+++ src/kcheckgmailcore.cpp
@@ -587,8 +587,6 @@ void KCheckGmailCore::slotSettingsChanged()
 			d->mJSP->retriever()->checkLoginParams();
 		}
 		
-		d->mJSP->retriever()->setInterval(Prefs::interval());
-		
 		if (Prefs::searchFor().length() == 0) {
 			Prefs::setSearchFor("in:inbox is:unread");
 			Prefs::self()->writeConfig();
@@ -624,6 +622,7 @@ void KCheckGmailCore::slotSettingsChanged()
 				QTimer::singleShot(100, this, SLOT(slotShowPrefsDialog()));
 			}
 		}
+		d->mJSP->retriever()->setInterval(Prefs::interval());
 	}
 
 	emit countColorChanged(Prefs::trayIconUnreadMessagesColor());
