netrunner-debian-kde-extras/kcheckgmail

commit ead5921312b07a7f49a2d47590149f282a380b1c

Replaces deprecated QTimer::changeInterval() by QTimer::setInterval()
Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

src/gmail.cpp
@@ -623,7 +623,7 @@ void GMail::setInterval(unsigned int i, bool forceStart)
 		else
 			running = mTimer->isActive();
 		
-		mTimer->changeInterval(MILLISECS(mInterval));
+		mTimer->setInterval(MILLISECS(mInterval));
 		
 		//Prevent starting the timer when it wasn't needed to
 		if(!running)

commit 68104fb2cbdd6769b29600301b88e45b6edb8bf3

Fixes a bug where the periodic check didn't work
This bug was introduced by commit ead5921312b07a7f49a2d47590149f282a380b1c

Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

src/gmail.cpp
@@ -645,7 +645,7 @@ void GMail::setInterval(unsigned int i, bool forceStart)
 		else
 			running = mTimer->isActive();
 		
-		mTimer->setInterval(MILLISECS(mInterval));
+		mTimer->start(MILLISECS(mInterval));
 		
 		//Prevent starting the timer when it wasn't needed to
 		if(!running)